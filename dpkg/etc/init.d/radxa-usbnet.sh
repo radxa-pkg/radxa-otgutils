ce381659d1af9b824c40a82f72fff954453b638c
Yuntian Zhang <yt@radxa.com>
2021-12-16 14:26:20 +0800

    Fix tabs and typo

--- dpkg/etc/init.d/radxa-usbnet.sh
+++ dpkg/etc/init.d/radxa-usbnet.sh
@@ -3,13 +3,13 @@
 configfs_init()
 {
     modprobe libcomposite
-	mkdir -p /sys/kernel/config/usb_gadget/radxa -m 0770
-	echo 0x1d6b > /sys/kernel/config/usb_gadget/radxa/idVendor
-	echo 0x0104 > /sys/kernel/config/usb_gadget/radxa/idProduct
-	mkdir -p /sys/kernel/config/usb_gadget/radxa/strings/0x409 -m 0770
-	echo "Radxa"  > /sys/kernel/config/usb_ga dget/radxa/strings/0x409/manufacturer
-	echo "Ethernet Control Model"  > /sys/kernel/config/usb_gadget/radxa/strings/0x409/product
-	mkdir -p /sys/kernel/config/usb_gadget/radxa/configs/r.1 -m 0770
+    mkdir -p /sys/kernel/config/usb_gadget/radxa -m 0770
+    echo 0x1d6b > /sys/kernel/config/usb_gadget/radxa/idVendor
+    echo 0x0104 > /sys/kernel/config/usb_gadget/radxa/idProduct
+    mkdir -p /sys/kernel/config/usb_gadget/radxa/strings/0x409 -m 0770
+    echo "Radxa"  > /sys/kernel/config/usb_gadget/radxa/strings/0x409/manufacturer
+    echo "Ethernet Control Model"  > /sys/kernel/config/usb_gadget/radxa/strings/0x409/product
+    mkdir -p /sys/kernel/config/usb_gadget/radxa/configs/r.1 -m 0770
 }
 
 function_init()
@@ -24,31 +24,33 @@ function_init()
 start()
 {
     configfs_init
-	function_init
+    function_init
 
-	UDC=$(ls /sys/class/udc/ | head -1)
-	echo $UDC > /sys/kernel/config/usb_gadget/radxa/UDC
+    set -e
+    UDC=$(ls /sys/class/udc/ | head -1)
+    echo $UDC > /sys/kernel/config/usb_gadget/radxa/UDC
 }
 
 stop()
 {
-	echo "none" > /sys/kernel/config/usb_gadget/radxa/UDC
+    set -e
+    echo "none" > /sys/kernel/config/usb_gadget/radxa/UDC
 }
 
 case "$1" in
 start)
-	start
-	;;
+    start
+    ;;
 stop)
     stop
-	;;
+    ;;
 restart|reload)
     stop
     start
-	;;
+    ;;
 *)
-	echo "Usage: $0 {start|stop|restart}"
-	exit 1
+    echo "Usage: $0 {start|stop|restart}"
+    exit 1
 esac
 
 exit 0
